47aea0f0165021fa70d6545e5612e6c26e6e9819
mustapha akhiyat <mustapha.akhiyat2@gmail.com>
2023-05-21 17:16:55 +0200

    edit the header and the footer

---
+++ resources/views/frontend/partials/Footer.blade.php
@@ -0,0 +1,19 @@
+<!-- Footer Section Begin -->
+<footer class="footer">
+
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <div class="footer__copyright__text">
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                <p>Copyright ©
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>-2024
+                    All rights reserved <i class="fa fa-heart-o" aria-hidden="true"></i>
+                </p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
